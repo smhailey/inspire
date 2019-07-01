190cdaa39e188256ece885a9f99f53c94e7a49f1
smhailey <smhailey@msn.com>
2019-07-01 15:07:43 -0600

    Background image displays properly

--- app/models/quote.js
+++ app/models/quote.js
@@ -7,13 +7,8 @@ export default class Quote {
   quoteTemplate() {
     return `
     <div class="" id="quote">
-			<h6 class="text-red mt-1">${this.body}</h6>
+			<h6 class="text-white mt-1">${this.body}</h6>
 			<p class="text-red mt-3"><span class="author"> &mdash; ${this.author}</span></p>
-			<div>
-				<button class="btn btn-sm btn-outline-light shadow-lg mt-3" id="next-quote"
-					onclick="app.controllers.quoteController.nextQuote()">Change quote<i class="fas fa-arrow-right ml-2"></i></button>
-			</div>
-			<p class="after-author"> </p>
 		</div>
     `
   }
